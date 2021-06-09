e1b9aca3b8b9b3cb108e18b469b811b4d4db420c
Krishna <82990200+Xrishna@users.noreply.github.com>
2021-06-09 06:55:22 +0530

    Update setting.py

--- sessionMaker/setting.py
+++ sessionMaker/setting.py
@@ -1,5 +1,5 @@
 class BasicSettings:
     API_ID: int =4463930
-    API_HASH: str =6d2ef6f4bee0296922278a3dbbf5b7d4
+    API_HASH: str ="6d2ef6f4bee0296922278a3dbbf5b7d4"
     BOT_TOKEN: str =1804086587:AAEEYqShtPL00JawvrkGdw5gcFtQcf16MUI
     LOG_CHANNEL: int =-1001176054220
